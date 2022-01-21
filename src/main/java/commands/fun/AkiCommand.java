9af7c034c3f4543efa852b922e11a4ea9919a9d0
superusertoor <lowercasetoor@protonmail.com>
2022-01-21 14:19:30 +0100

    master

--- src/main/java/commands/fun/AkiCommand.java
+++ src/main/java/commands/fun/AkiCommand.java
@@ -21,6 +21,7 @@ public class AkiCommand extends ListenerAdapter {
 
     private static HashMap<String, Akiwrapper.Answer> answers = new HashMap<>();
 
+
     static {
         answers.put("p", Akiwrapper.Answer.PROBABLY);
         answers.put("y", Akiwrapper.Answer.YES);
